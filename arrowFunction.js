1fa9abe42235019973e722c6c54c40350cd023ca
Abdullah Mashaan <eng.aalenezi@gmail.com>
2024-11-04 19:14:48 +0300

    Solved 3 tasks

---
+++ arrowFunction.js
@@ -0,0 +1,23 @@
+// Rewrite the following function as an arrow function:
+/******************************
+	function greet(name) {
+	  return `Hello, ${name}!`;
+	}
+********************************/
+
+
+
+ const greet = (name) => `Hello, ${name}!`;
+
+ console.log(greet("Abdullah"));
+
+// Write a simple arrow function that takes two parameters and returns their sum.
+
+const sum = (firstNum, secondNum) => firstNum + secondNum;
+
+console.log(sum(2, 4));
+
+// Write a simple arrow function that squares a number.
+
+const square = (num) => num**2;
+console.log(square(2));
